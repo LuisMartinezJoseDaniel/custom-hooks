0fa9c2e29c6dfbe89ebf43c7d5cb0df694002f43
Danny <josedan.jd94@gmail.com>
2022-07-26 18:20:08 -0500

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+//* Recuperar los todos
+//* init -> inicializa el initalState
+const init = () => {
+  const todos = localStorage.getItem("todos");
+  return JSON.parse(todos) || [];
+};
+
+export const useTodos = (initialState = []) => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    //* Crear la accion
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+    //* Mandar a llamar al reducer para despachar la
+    dispatch(action);
+  };
+
+  const handleRemoveTodo = (id) => {
+    const action = {
+      type: "[TODO] Remove Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+
+  return {
+    todos,
+    handleNewTodo,
+    handleRemoveTodo,
+    handleToggleTodo,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+  };
+};
